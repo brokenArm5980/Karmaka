6a59c0e6aa9b83a409a8c43ba161dd5785c92b77
Noe-Briffa <noebriffa37@gmail.com>
2024-01-06 15:38:36 +0100

    Update Jeu.java
    add test in Jeu

--- Karmaka/Jeu.java
+++ Karmaka/Jeu.java
@@ -1,5 +1,15 @@
 package Karmaka;
 
+import java.util.*;
+
 public class Jeu {
+	Queue<Integer> pile = new LinkedList<>();
 	
+	public void initialize() {
+		pile.add(0);
+		pile.add(1);
+		pile.add(2);
+		pile.add(3);
+		System.out.println(pile.element());
+	}
 }
