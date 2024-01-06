6ca2da1153581370e8787ca13bcb4e73cbca1c5e
Noe-Briffa <noebriffa37@gmail.com>
2024-01-06 16:04:44 +0100

    Update Jeu.java
    initialiser_source

--- Karmaka/Jeu.java
+++ Karmaka/Jeu.java
@@ -1,10 +1,10 @@
 package Karmaka;
 import java.util.*;
 public class Jeu {
-	
+	// Liste de cartes représentant la fosse
 	private List<Carte> source;
 
-	    // Liste de cartes dans la fosse
+	// Liste de cartes dans la fosse
 	private List<Carte> fosse;
 	   
 	private Joueur joueur1;
@@ -15,7 +15,134 @@ public class Jeu {
 	    this.fosse = new ArrayList<>();
 	    this.joueur1 = new Joueur();
 	    this.joueur2 = new Joueur();
+
+	    // initialisation de la source
+	    initialiser_source();
+	    
+	    
+	    
 	    }
+
+	// initialisation de la source
+	public void initialiser_source() {
+		Carte carte;
+	    
+	    // Transmigration
+	    carte = new Carte("Transmigration", Couleur.BLEU, 1);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	    // Coup d Oeil
+	    carte = new Carte("Coup d Oeil", Couleur.BLEU, 1);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	    // Destinee
+	    carte = new Carte("Destinee", Couleur.BLEU, 2);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Reves Brises
+	    carte = new Carte("Reves Brises", Couleur.BLEU, 2);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Deni
+	    carte = new Carte("Deni", Couleur.BLEU, 2);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Duperie
+	    carte = new Carte("Duperie", Couleur.BLEU, 3);
+	    for(int i = 0; i < 2; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Vol
+	    carte = new Carte("Vol", Couleur.BLEU, 3);
+	    for(int i = 0; i < 2; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Lendemain
+	    carte = new Carte("Lendemain", Couleur.VERT, 1);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Recyclage
+	    carte = new Carte("Recyclage", Couleur.VERT, 1);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Sauvetage
+	    carte = new Carte("Sauvetage", Couleur.VERT, 2);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Longevite
+	    carte = new Carte("Longevite", Couleur.VERT, 2);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Semis
+	    carte = new Carte("Semis", Couleur.VERT, 2);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Voyage
+	    carte = new Carte("Voyage", Couleur.VERT, 3);
+	    for(int i = 0; i < 2; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Jubile
+	    carte = new Carte("Jubile", Couleur.VERT, 3);
+	    for(int i = 0; i < 2; i++) {
+	    	this.source.add(carte);
+	    }
+	    // Panique
+	    carte = new Carte("Panique", Couleur.ROUGE, 1);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Dernier Souffle
+	    carte = new Carte("Dernier Souffle", Couleur.ROUGE, 1);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Crise
+	    carte = new Carte("Crise", Couleur.ROUGE, 2);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Roulette
+	    carte = new Carte("Roulette", Couleur.ROUGE, 2);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Fournaise
+	    carte = new Carte("Fournaise", Couleur.ROUGE, 2);
+	    for(int i = 0; i < 3; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Vengeance
+	    carte = new Carte("Vengeance", Couleur.ROUGE, 3);
+	    for(int i = 0; i < 2; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Bassesse
+	    carte = new Carte("Bassesse", Couleur.ROUGE, 3);
+	    for(int i = 0; i < 2; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Incarnation
+	    carte = new Carte("Incarnation", Couleur.MOSAIQUE, 1);
+	    for(int i = 0; i < 5; i++) {
+	    	this.source.add(carte);
+	    }
+	 	// Mimetisme
+	    carte = new Carte("Mimetisme", Couleur.MOSAIQUE, 2);
+	    for(int i = 0; i < 2; i++) {
+	    	this.source.add(carte);
+	    }
+	}
 
 	public void ajouterCarteSource(Carte carte) {
 	        this.source.add(carte);
@@ -26,7 +153,6 @@ public class Jeu {
 	
 	public static void main(String[] args) {
 	        Jeu monJeu = new Jeu();
-
 	    }
 
 }
