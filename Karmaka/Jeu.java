31a21adbb208d4f44fa4baf490507ef5d3f3e539
brokenArm5980 <mathieuh5980@gmail.com>
2024-01-06 15:39:34 +0100

    Update Jeu.java
    add class jeu

--- Karmaka/Jeu.java
+++ Karmaka/Jeu.java
@@ -1,5 +1,31 @@
 package Karmaka;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Jeu {
+	private List<Carte> source;
+
+	    // Liste de cartes dans la fosse
+	private List<Carte> fosse;
+	   
+	private Joueur joueur1;
+	private Joueur joueur2;
+
+	public Jeu() {
+		this.source = new ArrayList<>();
+	    this.fosse = new ArrayList<>();
+	    this.joueur1 = new Joueur();
+	    this.joueur2 = new Joueur();
+	    }
+
+	public void ajouterCarteSource(Carte carte) {
+	        this.source.add(carte);
+	    }
+	public void ajouterCarteFosse(Carte carte) {
+	        this.fosse.add(carte);
+	    }
 	
+	public static void main(String[] args) {
+	        Jeu monJeu = new Jeu();
+
+	    }
 }
